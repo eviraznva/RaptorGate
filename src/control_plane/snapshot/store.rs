7a00ab59333702365f021f18246478beecd2896e
mattys <53041694+mattys1@users.noreply.github.com>
2026-03-30 21:16:51 +0200

    fix(firewall): remove bad src (#330)

--- src/control_plane/snapshot/store.rs
+++
@@ -1,7 +0,0 @@
-use crate::control_plane::backend_api::proto::raptorgate::config::ConfigResponse;
-use crate::control_plane::error::SnapshotError;
-
-pub trait SnapshotStore: Send + Sync {
-    fn load(&self) -> Result<Option<ConfigResponse>, SnapshotError>;
-    fn save(&self, response: &ConfigResponse) -> Result<(), SnapshotError>;
-}
--- src/control_plane/validation_api/proto.rs
+++
@@ -1,5 +0,0 @@
-pub mod raptorgate {
-    pub mod control {
-        tonic::include_proto!("raptorgate.control");
-    }
-}
